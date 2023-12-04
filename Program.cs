c2355f5419d5f87fe94096b870b916b5c997d55a
Duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-12-04 16:30:16 -0500

    feat: :ambulance: fix exercise
    fix wrong exercise
    
    @duvanrg

--- Program.cs
+++ Program.cs
@@ -3,30 +3,48 @@
     private static void Main(string[] args)
     {
         bool run = true;
+        List<List<int>> nums = new List<List<int>>();
         while (run)
         {
-            Console.Clear();
             Console.ForegroundColor = ConsoleColor.White;
             try
             {
-                Console.Write("Ingrese primer numero: ");
-                int num_1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Ingrese segundo numero: ");
-                int num_2 = Convert.ToInt32(Console.ReadLine());
-                // otro ejemplo 2620 y 2924, 1184 y 1210, 5020 y 5564, 6232 y 6368
-                if (NumFriend(num_1) == num_2 && NumFriend(num_2) == num_1)
+
+                for (int i = 1000; i < 1500; i++)
                 {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine($"Los numeros {num_1} y {num_2} son numeros amistosos");
-                    Console.WriteLine($"(っ＾▿＾)۶🍸🌟🍺٩(˘◡˘ )\n");
+                    for (int j = 1000; j <= 1500; j++)
+                    {
+                        if (NumFriend(i) == j && NumFriend(j) == i)
+                        {
+                            if (!nums.Any(p => p.Contains(i) && p.Contains(j)))
+                            {
+                                List<int> num = new List<int> { i, j };
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.WriteLine($"Los numeros {i} y {j} son numeros amistosos");
+                                Console.WriteLine($"(っ＾▿＾)۶🍸🌟🍺٩(˘◡˘ )\n");
+                                nums.Add(num);
+                            }
+                        }
+                    }
                 }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                    Console.WriteLine($"Los numeros {num_1} y {num_2} NO son numeros amistosos");
-                    Console.WriteLine($"(ง︡'-'︠)ง(ง︡'-'︠)ง(ง︡'-'︠)ง");
-                }
-                run = false;
+                break;
+                //     Console.Write("Ingrese primer numero: ");
+                //     int num_1 = Convert.ToInt32(Console.ReadLine());
+                //     Console.Write("Ingrese segundo numero: ");
+                //     int num_2 = Convert.ToInt32(Console.ReadLine());
+                //     if (NumFriend(num_1) == num_2 && NumFriend(num_2) == num_1)
+                //     {
+                //         Console.ForegroundColor = ConsoleColor.Cyan;
+                //         Console.WriteLine($"Los numeros {num_1} y {num_2} son numeros amistosos");
+                //         Console.WriteLine($"(っ＾▿＾)۶🍸🌟🍺٩(˘◡˘ )\n");
+                //     }
+                //     else
+                //     {
+                //         Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                //         Console.WriteLine($"Los numeros {num_1} y {num_2} NO son numeros amistosos");
+                //         Console.WriteLine($"(ง︡'-'︠)ง(ง︡'-'︠)ง(ง︡'-'︠)ง");
+                //     }
+                //     run = false;
             }
             catch (FormatException e)
             {
